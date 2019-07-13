65158389dad0199bbe5094fe9950fd1b8ad238f3
ArjenCole <jinhesky@hotmail.com>
2019-07-13 17:11:42 +0800

    初步搭建页面

--- pages/score/score.js
+++ pages/score/score.js
@@ -1,66 +1,28 @@
-// pages/score/score.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    showTopTips: false,
 
+    isAgree: false
+  },
+  showTopTips: function () {
+    var that = this;
+    this.setData({
+      showTopTips: !that.data.isAgree
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  bindAgreeChange: function (e) {
+    console.log(e)
+    console.log(!!e.detail.value.length)
+    this.setData({
+      isAgree: !!e.detail.value.length
+    });
+    console.log(this.data.isAgree)
 
   }
-})
+});
